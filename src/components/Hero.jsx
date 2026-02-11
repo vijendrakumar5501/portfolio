8cb8b9cc69a099c64289b695799a963a067f10cb
vijendra <vijendrakumar5501@gmail.com>
2026-02-11 17:01:02 +0530

    added hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,81 @@
+import { motion } from "framer-motion";
+import vk from "../assets/vk.jpg"
+
+export default function Hero() {
+  return (
+    <section className="min-h-screen flex flex-col items-center justify-center text-center px-6">
+
+    
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8 }}
+        className="relative"
+      >
+        <motion.div
+          animate={{ y: [0, -15, 0] }}
+          transition={{ repeat: Infinity, duration: 3 }}
+          className="w-44 h-44 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 p-1"
+        >
+          <img
+            src={vk}
+            className="rounded-full contain-content w-40 h-40"
+          />
+        </motion.div>
+      </motion.div>
+
+  
+      <motion.h1
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.3 }}
+        className="text-4xl md:text-5xl font-bold mt-6"
+      >
+        Hi, I'm Vijendra Kumar
+      </motion.h1>
+
+   
+      <motion.p
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5 }}
+        className="text-xl text-gray-400 mt-2"
+      >
+        Frontend Developer
+      </motion.p>
+
+     
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.7 }}
+        className="max-w-xl text-gray-500 mt-4"
+      >
+        I build modern web applications using React, Tailwind & JavaScript.
+      </motion.p>
+
+     
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.9 }}
+        className="flex gap-4 mt-6"
+      >
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          className="px-6 py-2 border rounded hover:bg-white hover:text-black"
+        >
+          Download CV
+        </motion.button>
+
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          className="px-6 py-2 border rounded hover:bg-white hover:text-black"
+        >
+          Let’s Collaborate
+        </motion.button>
+      </motion.div>
+
+    </section>
+  );
+}
